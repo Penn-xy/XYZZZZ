5e41b8155d91a131d5541f2f4294b88dd539441a
xudegithub <xu_9308@163.com>
2016-06-21 22:23:51 +0800

    keyboardManager

---
+++ XYZZZZ/XYKit/XYKeyboardManager.h
@@ -0,0 +1,61 @@
+//
+//  XYKeyboardManager.h
+//  XYZZZZ
+//
+//  Created by xu on 16/6/21.
+//  Copyright © 2016年 Yupeng. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef struct {
+    BOOL fromVisible;
+    BOOL toVisible;
+    CGRect fromFrame;
+    CGRect toFrame;
+    NSTimeInterval animationDuration;
+    UIViewAnimationCurve animationCurve;
+    UIViewAnimationOptions animationOption;
+} XYKeyboardTransition;
+
+@protocol XYKeyboardObserver <NSObject>
+@optional
+- (void)keyboardChangedWithTransition:(XYKeyboardTransition)transition;
+@end
+
+
+@interface XYKeyboardManager : NSObject
+
+- (instancetype)init;
++ (instancetype)new;
+
++ (instancetype)defaultManager;
+
+@property (nullable, nonatomic, readonly) UIWindow *keyboardWindow;
+
+@property (nullable, nonatomic, readonly) UIView *keyboardView;
+
+@property (nonatomic, readonly, getter=isKeyboardVisible) BOOL keyboardVisible;
+
+@property (nonatomic, readonly) CGRect keyboardFrame;
+
+- (void)addObserver:(id<XYKeyboardObserver>)observer;
+
+- (void)removeObserver:(id<XYKeyboardObserver>)observer;
+
+- (CGRect)convertRect:(CGRect)rect toView:(nullable UIView *)view;
+
+@end
+
+
+
+
+
+
+
+
+
+NS_ASSUME_NONNULL_END
---
+++ XYZZZZ/XYKit/XYKeyboardManager.m
@@ -0,0 +1,15 @@
+//
+//  XYKeyboardManager.m
+//  XYZZZZ
+//
+//  Created by xu on 16/6/21.
+//  Copyright © 2016年 Yupeng. All rights reserved.
+//
+
+#import "XYKeyboardManager.h"
+#import <objc/runtime.h>
+
+
+@implementation XYKeyboardManager
+
+@end
