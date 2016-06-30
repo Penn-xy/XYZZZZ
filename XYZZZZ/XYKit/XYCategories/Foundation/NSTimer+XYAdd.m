5087b13c89b08d48f49a2e84b436a597d790ece0
xudegithub <yupeng@yangmideMac-mini.local>
2016-06-30 09:09:44 +0800

    c

--- XYZZZZ/XYKit/XYCategories/Foundation/NSTimer+XYAdd.m
+++ XYZZZZ/XYKit/XYCategories/Foundation/NSTimer+XYAdd.m
@@ -7,7 +7,28 @@
 //
 
 #import "NSTimer+XYAdd.h"
+#import "XYCategoriesMacro.h"
+
+//XYSYNTH_DUMMY_CLASS(NSTimer_XYAdd)
+
+
 
 @implementation NSTimer (XYAdd)
+
++ (void)_yy_ExecBlock:(NSTimer *)timer {
+    if ([timer userInfo]) {
+        void (^block)(NSTimer *timer) = (void (^)(NSTimer *timer))[timer userInfo];
+        block(timer);
+    }
+}
+
++ (NSTimer *)scheduledTimerWithTimeInterval:(NSTimeInterval)seconds block:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats {
+    return [NSTimer scheduledTimerWithTimeInterval:seconds target:self selector:@selector(_yy_ExecBlock:) userInfo:[block copy] repeats:repeats];
+}
+
++ (NSTimer *)timerWithTimeInterval:(NSTimeInterval)seconds block:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats {
+    return [NSTimer timerWithTimeInterval:seconds target:self selector:@selector(_yy_ExecBlock:) userInfo:[block copy] repeats:repeats];
+}
+
 
 @end
