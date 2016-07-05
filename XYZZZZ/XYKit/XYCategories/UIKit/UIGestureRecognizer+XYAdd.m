6e99c08486aed8f0a30af00dc11d0f1ce85ea003
xudegithub <yupeng@yangmideMac-mini.local>
2016-07-05 09:06:00 +0800

    part_categoryOfUIKit

--- XYZZZZ/XYKit/XYCategories/UIKit/UIGestureRecognizer+XYAdd.m
+++ XYZZZZ/XYKit/XYCategories/UIKit/UIGestureRecognizer+XYAdd.m
@@ -7,7 +7,71 @@
 //
 
 #import "UIGestureRecognizer+XYAdd.h"
+#import "XYCategoriesMacro.h"
+#import <objc/runtime.h>
+
+static const int block_key;
+
+@interface _XYUIGestureRecognizerBlockTarget : NSObject
+
+@property (nonatomic, copy) void (^block)(id sender);
+
+- (id)initWithBlock:(void (^)(id sender))block;
+- (void)invoke:(id)sender;
+
+@end
+
+@implementation _XYUIGestureRecognizerBlockTarget
+
+- (id)initWithBlock:(void (^)(id sender))block{
+    self = [super init];
+    if (self) {
+        _block = [block copy];
+    }
+    return self;
+}
+
+- (void)invoke:(id)sender {
+    if (_block) _block(sender);
+}
+
+@end
+
+
+
 
 @implementation UIGestureRecognizer (XYAdd)
+
+- (instancetype)initWithActionBlock:(void (^)(id sender))block {
+    self = [self init];
+    [self addActionBlock:block];
+    return self;
+}
+
+- (void)addActionBlock:(void (^)(id sender))block {
+    _XYUIGestureRecognizerBlockTarget *target = [[_XYUIGestureRecognizerBlockTarget alloc] initWithBlock:block];
+    [self addTarget:target action:@selector(invoke:)];
+    NSMutableArray *targets = [self _xy_allUIGestureRecognizerBlockTargets];
+    [targets addObject:target];
+}
+
+- (void)removeAllActionBlocks{
+    NSMutableArray *targets = [self _xy_allUIGestureRecognizerBlockTargets];
+    [targets enumerateObjectsUsingBlock:^(id target, NSUInteger idx, BOOL *stop) {
+        [self removeTarget:target action:@selector(invoke:)];
+    }];
+    [targets removeAllObjects];
+}
+
+- (NSMutableArray *)_xy_allUIGestureRecognizerBlockTargets {
+    NSMutableArray *targets = objc_getAssociatedObject(self, &block_key);
+    if (!targets) {
+        targets = [NSMutableArray array];
+        objc_setAssociatedObject(self, &block_key, targets, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return targets;
+}
+
+
 
 @end
