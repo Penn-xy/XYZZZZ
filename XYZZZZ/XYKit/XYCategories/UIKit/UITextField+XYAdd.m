f271390e79a74633bf1734ed145875dabb582710
xudegithub <yupeng@yangmideMac-mini.local>
2016-07-06 09:05:10 +0800

    category_done

--- XYZZZZ/XYKit/XYCategories/UIKit/UITextField+XYAdd.m
+++ XYZZZZ/XYKit/XYCategories/UIKit/UITextField+XYAdd.m
@@ -7,7 +7,27 @@
 //
 
 #import "UITextField+XYAdd.h"
+#import "XYCategoriesMacro.h"
+
+XYSYNTH_DUMMY_CLASS(UITextField_XYAdd)
+
 
 @implementation UITextField (XYAdd)
+
+
+- (void)selectAllText {
+    UITextRange *range = [self textRangeFromPosition:self.beginningOfDocument toPosition:self.endOfDocument];
+    [self setSelectedTextRange:range];
+}
+
+- (void)setSelectedRange:(NSRange)range {
+    UITextPosition *beginning = self.beginningOfDocument;
+    UITextPosition *startPosition = [self positionFromPosition:beginning offset:range.location];
+    UITextPosition *endPosition = [self positionFromPosition:beginning offset:NSMaxRange(range)];
+    UITextRange *selectionRange = [self textRangeFromPosition:startPosition toPosition:endPosition];
+    [self setSelectedTextRange:selectionRange];
+}
+
+
 
 @end
